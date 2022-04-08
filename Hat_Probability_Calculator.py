3b6ec6ce62a6724c541e94b8be78f70917f14491
Gioack <101208747+Gioack@users.noreply.github.com>
2022-04-08 21:47:23 +0200

    Code works well

--- Hat_Probability_Calculator.py
+++ Hat_Probability_Calculator.py
@@ -1,16 +1,16 @@
-# python Probability_Calculator.py
+# python Hat_Probability_Calculator.py
 import copy
 import random
 class Hat:
-    def __init__(self, balls):
+
+    def __init__(self, **balls):
         self.contents = list()
-        balls = str(balls.replace(":","="))
-        print(balls)
+        # print(balls)
         for x in balls:
             while balls[x] > 0:
                 self.contents.append(x)
                 balls[x] = balls[x] - 1
-        print(self.contents)
+        # print(self.contents)
     def draw(self, num_balls):
         # random.seed(0)
         if num_balls > len(self.contents):
@@ -18,8 +18,7 @@ class Hat:
         draw = random.sample(self.contents, num_balls)
         return draw
 # hat = Hat({"red":1, "green":1})
-hat2 = Hat(yellow=3, blue=2, green=6)
-print(hat.draw(6))
+hat2 = Hat(blue=6, green=11, white = 2, black = 1)
 # for x in hat:
     # print(x)
 
@@ -30,11 +29,24 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
         draw = hat.draw(num_balls_drawn)
         for ball in draw:
             draw_dict[ball] = draw_dict.get(ball,0) + 1
-        if draw_dict == expected_balls:
+        number_colors_matched = 0
+        for color in expected_balls:
+            if color in draw_dict:
+                if draw_dict[color] >= expected_balls[color]:
+                    number_colors_matched = number_colors_matched + 1
+        if number_colors_matched == len(expected_balls):
             times_matched = times_matched + 1
 
+        #
+        # number_colors_matched = 0
+        # for color in expected_balls:
+        #     if color in draw_dict:
+        #         if expected_balls[color] >= draw_dict[color]:
+        #             number_colors_matched = number_colors_matched + 1
+        # if number_colors_matched == len(expected_balls):
+        #     times_matched = times_matched + 1
     return times_matched/num_experiments
-print(experiment(hat,
-{"green":1},
+print(experiment(hat2,
+{"black":1},
 1,
-10000))
+100000))
