19fe0f76977a82a1ccacd4265b89b006ce5fd4a3
Gioack <101208747+Gioack@users.noreply.github.com>
2022-04-17 12:21:05 +0200

    made the whole more pythonic

--- Arithmetic_Formatter.py
+++ Arithmetic_Formatter.py
@@ -14,40 +14,40 @@ def arithmetic_arranger(problems, Show_result = False):
     all = ""
     for each_problem in problems:
         Where_plus_or_minus = max(each_problem.find('+'), each_problem.find('-'))
-        each_problem_part1 = "  " + each_problem[:Where_plus_or_minus].strip()
-        each_problem_part2 = each_problem[Where_plus_or_minus]+ " " + each_problem[Where_plus_or_minus + 1:].strip()
+        each_problem_part1 = f'  {each_problem[:Where_plus_or_minus].strip()}'
+        each_problem_part2 = f'{each_problem[Where_plus_or_minus]} {each_problem[Where_plus_or_minus + 1:].strip()}'
         # The code above looks like this and not simply like: each_problem_part2 = each_problem[Where_plus_or_minus:]
         # because of cases like "     3 +     3", it makes these situations completely fine
         if len(each_problem_part1) >= len(each_problem_part2):
             while len(each_problem_part1) > len(each_problem_part2):
-                each_problem_part2 = each_problem_part2[:2] + " " + each_problem_part2[2:]
+                each_problem_part2 = f"{each_problem_part2[:2]} {each_problem_part2[2:]}"
         if len(each_problem_part2) > len(each_problem_part1):
             while len(each_problem_part2) > len(each_problem_part1):
-                each_problem_part1 = " " + each_problem_part1
+                each_problem_part1 = f" {each_problem_part1}"
         if (allthefirstpart == ""):
             allthefirstpart = each_problem_part1
             allthesecondpart = each_problem_part2
             for digit in range(len(each_problem_part1)):
-                Dashes = Dashes + '-'
+                Dashes += '-'
         else:
-            allthefirstpart = allthefirstpart + "    " + each_problem_part1
-            allthesecondpart = allthesecondpart + "    " + each_problem_part2
-            Dashes = Dashes + "    "
+            allthefirstpart += f'    {each_problem_part1}'
+            allthesecondpart += f'    {each_problem_part2}'
+            Dashes += "    "
             for digit in range(len(each_problem_part1)):
-                Dashes = Dashes + '-'
+                Dashes += '-'
         if Show_result == True:
             if '+' in each_problem:
                 Result_of_2parts = str(int(each_problem_part1.strip()) + int(each_problem_part2[2:].strip()))
             else:
                 Result_of_2parts = str(int(each_problem_part1.strip()) - int(each_problem_part2[2:].strip()))
             while len(Result_of_2parts) < len(each_problem_part1):
-                Result_of_2parts = " " +  Result_of_2parts
+                Result_of_2parts = f" {Result_of_2parts}"
             if (Results == ""):
                 Results = Result_of_2parts
             else:
-                Results = Results + "    " + Result_of_2parts
-            all = "\n" + Results
-    all = allthefirstpart + "\n" + allthesecondpart + "\n" + Dashes + all
+                Results += f'    {Result_of_2parts}'
+            all = f"\n{Results}"
+    all = f"{allthefirstpart}\n{allthesecondpart}\n{Dashes}{all}"
     return all
 def Control_length(problems):
     if len(problems) > 5:
@@ -71,4 +71,4 @@ def Control_length_numbers(problems):
             number = number.strip()
             if len(number) > 4:
                 return True
-print(arithmetic_arranger(["780 + 56", "3849 - 1784", "305 + 489"]))
+print(arithmetic_arranger(["780 + 56", "3849 - 1784", "305 + 489"],True))
