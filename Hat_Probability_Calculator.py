8dbe3b117714823be85c67d6c995dd97cb2ce415
Gioack <101208747+Gioack@users.noreply.github.com>
2022-04-17 12:10:36 +0200

    made code simpler, fixed bug in experiment

--- Hat_Probability_Calculator.py
+++ Hat_Probability_Calculator.py
@@ -1,4 +1,3 @@
-# python Hat_Probability_Calculator.py
 import copy
 import random
 from collections import Counter
@@ -18,14 +17,16 @@ class Hat:
         return draw
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments=75000):
     times_matched = 0
-    for time in range(num_experiments):
+    for _ in range(num_experiments):
         hat_copy = copy.deepcopy(hat)
         draw = hat_copy.draw(num_balls_drawn)
         draw_dict = Counter(draw)
+        # print(draw_dict)
+        # print(expected_balls.items())
         number_colors_matched = 0
-        for (color_expected, number_expected), (color_drawn, number_drawn) in zip(expected_balls.items(), draw_dict.items()):
+        for (color_expected, number_expected) in expected_balls.items():
             if color_expected in draw_dict:
-                if number_drawn >= number_expected:
+                if draw_dict[color_expected] >= number_expected:
                     number_colors_matched += 1
         if number_colors_matched == len(expected_balls):
             times_matched += 1
@@ -33,4 +34,5 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments=75000):
 #calls
 print(experiment(hat = Hat(blue=6, green=11, white = 2, black = 1),
 expected_balls = {"black":1,"green":3},
-num_balls_drawn = 5))
+num_balls_drawn = 5,
+num_experiments=1000))
